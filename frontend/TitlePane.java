1e4158458279a95c38272e2cb519aeeab60a51f0
Samuel Bergeron <32581690+TheBerg974@users.noreply.github.com>
2019-11-02 15:38:45 -0400

    Create TitlePane.java

---
+++ frontend/TitlePane.java
@@ -0,0 +1,28 @@
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+
+public class TitlePane extends Pane {
+
+    private Label title;
+    public double width;
+    public double height;
+
+    public TitlePane(double width, double height) {
+        this.width = width;
+        this.height = height;
+
+        this.setPrefSize(width, height);
+
+        double w = (this.getWidth()/2)-(title.getWidth()/2);
+        double h = (this.getHeight()/2)-(title.getHeight()/2);
+
+        this.title = new Label("Ising Model Simulation");
+        this.title.setFont(new Font(20));
+        this.title.setLayoutX(w);
+        this.title.setLayoutY(h);
+
+        this.getChildren().addAll(this.title);
+
+    }
+}
