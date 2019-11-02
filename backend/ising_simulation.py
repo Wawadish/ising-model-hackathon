869605832f16a7a2f434cbd9cc38d21180a3d052
Totom3 <suptom3@gmail.com>
2019-11-02 13:20:50 -0400

    Added data parsing to backend

--- backend/ising_simulation.py
+++ backend/ising_simulation.py
@@ -1,4 +1,4 @@
-import argparse 
+import argparse
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('rows', type=int, help='Number of rows')
@@ -7,5 +7,23 @@ parser.add_argument('gamma', type=float, help='The gamma ratio')
 parser.add_argument('data', type=str, help='el data')
 
 args = parser.parse_args()
-print('Rows: {}\n Columns: {}\n Gamma: {}\n Data: {}"
+print('Rows: {}\nColumns: {}\nGamma: {}\nData: {}\n'
 		.format(args.rows, args.cols, args.gamma, args.data))
+		
+num_rows = args.rows
+num_cols = args.cols
+gamma = args.gamma
+raw_data = args.data
+
+grid = [[0 for j in range(num_cols)] for i in range(num_rows)]
+
+for idx, val in enumerate(raw_data):
+	if val != '1':
+		continue
+
+	row = idx // num_cols
+	col = idx % num_cols
+	grid[row][col] = 1
+
+for row in grid:
+	print(''.join(str(x) for x in row))
