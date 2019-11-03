f6029657d15858345ee13852ce0546ffdf6f974f
Wawadish <wawadish@gmail.com>
2019-11-03 06:18:46 -0500

    Changed how predicat phase works

--- src/backend/predict_phase.py
+++ src/backend/predict_phase.py
@@ -7,24 +7,31 @@ class Object(object):
         pass
 
 dirpath = os.path.dirname(os.path.abspath(__file__))
-
 MODEL = dirpath + '/../models/model_20191103-015907'
-
+model = keras.models.load_model(MODEL)
 params = Object()
 params.num_cols = 100
 params.num_rows = 100
-state = [[0 for j in range(params.num_cols)] for i in range(params.num_rows)]
+while(True):
+        try:
+                f = open(dirpath+'/temp_ai', 'r')
+                for row in range(params.num_rows):
+                        row_str = f.readline()[:-1]
+                        for col, val in enumerate(row_str):
+                                state[row][col] = 1 if val == '1' else -1
+        except:
+                continue
+        
+        os.remove(dirpath+'/temp_ai')
+        
+        state = [[0 for j in range(params.num_cols)] for i in range(params.num_rows)]
 
-f = open(dirpath+'/temp_ai', 'r')
-for row in range(params.num_rows):
-	row_str = f.readline()[:-1]
-	for col, val in enumerate(row_str):
-		state[row][col] = 1 if val == '1' else -1
+        test_x = np.array(state).reshape((1, 100*100,))
+        print(test_x.shape)
 
-test_x = np.array(state).reshape((1, 100*100,))
-print(test_x.shape)
 
-model = keras.models.load_model(MODEL)
-pred_y = model.predict([test_x])
-output = np.argmax(pred_y)
-print("Prediction {}".format(output))
+        pred_y = model.predict([test_x])
+        output = np.argmax(pred_y)
+        print("Prediction {}".format(output))
+        
+
